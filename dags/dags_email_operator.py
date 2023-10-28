e84efd9f2315e4586a8825d21b9b347e4bea7f0b
Unho Chang <capcock@snu.ac.kr>
2023-10-28 10:26:29 +0900

    이메일 크론 스케줄 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.email import EmailOperator
 
 with DAG(
     dag_id='dags_email_operator',
-    schedule='0 8 * * *',
+    schedule='20 0 * * *',
     start_date=pendulum.datetime(2023, 10, 28, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
