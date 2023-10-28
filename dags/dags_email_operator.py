4407a09c5c910ae1a11afff6dcb3ca6c66e262f4
Unho Chang <capcock@snu.ac.kr>
2023-10-28 09:06:34 +0900

    날짜 오류 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.email import EmailOperator
 with DAG(
     dag_id='dags_email_operator',
     schedule='0 8 * * *',
-    start_date=pendulum.datetime(2003, 10, 28, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2023, 10, 28, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     send_email_task = EmailOperator(
